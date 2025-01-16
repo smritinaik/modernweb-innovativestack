18ca646166010a88250d8f695f5073b6e06e8dab
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-01-16 15:05:20 +0530

    gemini effect
    gemini effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import {SparklesPreview} from "@/app/components/SparklesPreview"
 import ThreeDCardDemo from "./components/card";
+import { GoogleGeminiEffectDemo } from "./components/Scroll";
 
 export default function Home() {
   return (
     <>
     <SparklesPreview/>
     <ThreeDCardDemo/>
+    <GoogleGeminiEffectDemo/>
     </>
   );
 }
